5fd414d6bd6e48b2a02ac4ed18d05a4095da2b3e
Valenchongo <107526652+Valenchongo@users.noreply.github.com>
2022-06-19 22:47:30 -0300

    Add files via upload

---
+++ PerlaShop.js
@@ -0,0 +1,9 @@
+nav = document.querySelector(".mobile"); 
+boton = document.querySelector(".toogle");
+
+boton.addEventListener("click",() => {
+    nav.classList.toggle("mobilenav");
+   
+});
+
+ 
